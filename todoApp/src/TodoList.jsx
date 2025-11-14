15c892fa5567e34dac68bffaada4acc1be0f6a2d
Yash Karande <yashkarande4240@gmail.com>
2025-11-14 19:46:02 +0530

    todo app created

---
+++ todoApp/src/TodoList.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function TodoList() {
+  let [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodos(prev => [...prev, { task: newTodo, id: uuidv4() }]);
+    setNewTodo("");
+  };
+
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  let deleteTodo = (id) => {
+    setTodos(prevTodos =>
+      prevTodos.filter(todo => todo.id !== id)
+    );
+  };
+
+  let upperCaseAll = () => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo => ({
+        ...todo,
+        task: todo.task.toUpperCase()
+      }))
+    );
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder='add a task'
+        value={newTodo}
+        onChange={updateTodoValue}
+      />
+
+      <br /><br />
+      <button onClick={addNewTask}>Add Task</button>
+
+      <hr />
+      <h4>Tasks Todo</h4>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <span>{todo.task}</span>
+            &nbsp; &nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+
+      <br /><br />
+      <button onClick={upperCaseAll}>Upper Case All</button>
+    </div>
+  );
+}

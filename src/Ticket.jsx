b23d18e4e2b4100b7b55467578e6a8e9ae2c7bf1
Yash Karande <yashkarande4240@gmail.com>
2025-11-16 23:51:11 +0530

    added style to ticket component

--- src/Ticket.jsx
+++ src/Ticket.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import TicketNum from './TicketNum'
+import "./Ticket.css"
 export default function Ticket({ticket}) {
   return (
-    <div>
+
+    <div className='Ticket'>
+      <p>Ticket</p>
       {ticket.map((num, idx) => (
         <Ticket num={num} key={idx}/>
       ))}

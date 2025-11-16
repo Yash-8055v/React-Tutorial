67e98db9e7fa135d45519e0074085c0c66ae780f
Yash Karande <yashkarande4240@gmail.com>
2025-11-16 23:48:52 +0530

    created ticket component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ProductTab from "./ProductTab";
 import "./App.css";
 // import "./index.css";
 import Counter from "./Counter";
+import Ticket from "./Ticket";
 
 import Lottery from "./Lottery";
 

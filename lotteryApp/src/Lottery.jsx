9d8cd768646140a4db0c1e5503dc840af929dca1
Yash Karande <yashkarande4240@gmail.com>
2025-11-24 09:49:21 +0530

    Lottery component modified

--- lotteryApp/src/Lottery.jsx
+++ lotteryApp/src/Lottery.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import "./Lottery.css"; 
 import { genTicket, sum } from './helper.js';
+import Ticket from './Ticket.jsx';
 
-export default function Lottery() {
-  let [ticket, setTicket] = useState(genTicket(3));
+export default function Lottery({n = 3, winningSum = 15}) {
+  let [ticket, setTicket] = useState(genTicket(n));
   let isWinning = sum(ticket) === 15;
 
   let buyTicket = () => {
@@ -14,12 +15,7 @@ export default function Lottery() {
   return (
     <div>
       <h1>Lottery Game!</h1>
-      <div className="ticket">
-        <span>{ticket[0]}</span>
-        <span>{ticket[1]}</span>
-        <span>{ticket[2]}</span>
-      </div>
-      <br />
+      <Ticket ticket={ticket}/>
       <button onClick={buyTicket}>Buy New Ticket</button>
       <h3>{isWinning && "Congratulation, You Won!!"}</h3>
     </div>

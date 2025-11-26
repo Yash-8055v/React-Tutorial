a69ddd8dded5933858ed69d9278a682dfc145b41
Yash Karande <yashkarande4240@gmail.com>
2025-11-26 08:21:32 +0530

    handle multiple data

--- react-Form/src/Form.jsx
+++ react-Form/src/Form.jsx
@@ -1,16 +1,41 @@
 import React, { useState } from 'react'
 
 export default function Form() {
-  let [name, setName] = useState("");
-  let handleName = (event) =>  {
-    setName(event.target.value);
+  let [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+  let handleInputChange = (event) =>  {
+    let fieldName = event.target.name;
+    let newValue = event.target.value;
+
+    setFormData((currData) => {
+      // currData[fieldName] = newValue;
+      // return {...currData}; // we need change whole obj to reflect new changes 
+
+      return {...currData, [fieldName]: newValue};
+    })
   }
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    useState({
+    name: "",
+    email: "",
+  });
+  }
+
   return (
     <div>
-      <form action="#">
-        <label htmlFor="name">Name</label>
-        <input type="text" id='name' placeholder='enter name' value={name} onChange={handleName} />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id='name' placeholder='enter name' value={formData.name} onChange={handleInputChange} name='name' />
         <br /><br />
+
+        <label htmlFor="email">Email: </label>
+        <input type="email" id='email' placeholder='enter email' value={formData.email} onChange={handleInputChange} name='email'/>
+        <br /><br />
+
         <button>Submit</button>
       </form>
     </div>
